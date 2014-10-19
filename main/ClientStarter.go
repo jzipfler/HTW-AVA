7ab5ca96d73d872eb62e9d8f8288651f6ac966cb
Jan Zipfler <zipfler.j@gmail.com>
2014-10-19 14:56:34 +0200

    Implemented the first tries to the ClientStarter.

--- main/ClientStarter.go
+++ main/ClientStarter.go
@@ -15,25 +15,51 @@ var (
 )
 
 func main() {
-	initializeLogger("LOG::: ")
+	//initializeLogger("LOG::: ")
 	client := client.New()
 	serverObject := server.New()
-	logger.Printf("%T\n", client)
-	logger.Printf("%T\n", serverObject)
-	logger.Println("Client: " + client.String() + "\nServer: " + serverObject.String())
+	printMessage("Client: " + client.String() + "\n\t\tServer: " + serverObject.String())
 	error := client.SetIpAddressAsString("1.2.3.4")
 	if error != nil {
-		logger.Fatalln(error)
+		printMessage(error.Error())
 		return
 	}
 	client.SetClientName("First")
-	logger.Println(client)
+	printMessage(client.String())
+
+	doServerStuff(serverObject)
 
 	printAndClearLoggerContent()
 }
 
+func doServerStuff(serverObject server.NetworkServer) {
+	serverObject.SetClientName("Server1")
+	serverObject.SetIpAddressAsString("127.0.0.1")
+	serverObject.SetPort(15108)
+	serverObject.SetUsedProtocol("tcp")
+	printMessage(serverObject.String())
+	err := server.StartServer(serverObject, logger)
+	if err != nil {
+		return
+	}
+	defer server.StopServer()
+	//for {
+	//	connection := server.ReceiveMessage()
+	//	printMessage(connection.LocalAddr().String())
+	//}
+}
+
+// Creates a new logger which uses a buffer where he collects the messages.
 func initializeLogger(preface string) {
 	logger = log.New(&loggingBuffer, preface, log.Lshortfile)
+}
+
+func printMessage(message string) {
+	if logger == nil {
+		log.Println(message)
+	} else {
+		logger.Println(message)
+	}
 }
 
 func printAndClearLoggerContent() {

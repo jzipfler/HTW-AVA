3493e210e1a849fa5e5f105d1500da09b13a5820
Jan Zipfler <zipfler.j@gmail.com>
2014-10-21 21:45:14 +0200

    Renamed the ClientStarter to Starter because it acts as a client and a server. (Independent node)

--- main/Starter.go
+++ main/Starter.go
@@ -3,18 +3,43 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"github.com/jzipfler/htw/ava/client"
 	"github.com/jzipfler/htw/ava/server"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var (
 	loggingBuffer bytes.Buffer
 	logger        *log.Logger
+	id            int
+	ipAddress     string
+	port          int
+	nodeListFile  string
 )
 
+// The ini function is called before the main function is started.
+// It is used to do some stuff that should be done before the rest
+// of the application starts and on what they may depend on.
+func init() {
+	flag.StringVar(&nodeListFile, "nodeList", "path/to/nodeList.txt", "A file where nodes are defined as follows: \"ID IP_ADDR:PORT\"")
+	flag.IntVar(&id, "id", 1, "The if of the actual starting node.")
+	flag.StringVar(&ipAddress, "ipAddress", "127.0.0.1", "The ip address of the actual starting node.")
+	flag.IntVar(&port, "port", 15108, "The port of the actual starting node.")
+}
+
+// The main function is used when the programm is called / executed.
 func main() {
+	if len(os.Args) == 1 {
+		flag.Usage()
+		os.Exit(0)
+	}
+	flag.Parse()
 	//initializeLogger("LOG::: ")
 	client := client.New()
 	serverObject := server.New()
@@ -30,6 +55,13 @@ func main() {
 	doServerStuff(serverObject)
 
 	printAndClearLoggerContent()
+
+	go signalHandler()
+
+	for {
+		fmt.Println("Test")
+		time.Sleep(1 * time.Second)
+	}
 }
 
 func doServerStuff(serverObject server.NetworkServer) {
@@ -66,5 +98,21 @@ func printAndClearLoggerContent() {
 	if loggingBuffer.Len() != 0 {
 		fmt.Println(&loggingBuffer)
 		loggingBuffer.Reset()
+	}
+}
+
+func signalHandler() {
+	c := make(chan os.Signal, 1)
+	// Signal für CTRL-C abfangen...
+	signal.Notify(c, syscall.SIGINT)
+
+	// foreach signal received
+	for signal := range c {
+		fmt.Println("\nSignal empfangen...")
+		fmt.Println(signal.String())
+		switch signal {
+		case syscall.SIGINT:
+			os.Exit(0)
+		}
 	}
 }

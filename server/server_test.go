321ad021a0abed81935693faf57430d3db6b7818
Jan Zipfler <zipfler.j@gmail.com>
2014-10-21 21:47:05 +0200

    Enhaced the testing of the server package.

--- server/server_test.go
+++ server/server_test.go
@@ -12,12 +12,16 @@ import (
 // Tests if the portranges are corret implemented
 func TestServerPortRange(t *testing.T) {
 	serverObject := server.New()
-	if error := serverObject.SetPort(-1); error == nil {
-		t.Error("Port has not to be lower than " + strconv.Itoa(server.MINIMUM_PORT) + ".")
-	} else {
-		t.Log(error)
+	toLow := -1
+	if error := serverObject.SetPort(toLow); error == nil {
+		t.Error("A port of " + strconv.Itoa(toLow) + " is not allowed to exists, because the minimal port number is " + strconv.Itoa(server.MINIMUM_PORT) + " .")
 	}
-	if error := serverObject.SetPort(151088); error == nil {
-		t.Error("Port has not to be higher than" + strconv.Itoa(server.MAXIMUM_PORT) + ".")
+	correctPortSize := 15108
+	if error := serverObject.SetPort(correctPortSize); error != nil {
+		t.Error("A port of " + strconv.Itoa(correctPortSize) + " should not return an error, because it is in a correct port range.")
+	}
+	toHigh := 151088
+	if error := serverObject.SetPort(toHigh); error == nil {
+		t.Error("A port of " + strconv.Itoa(toHigh) + " is not allowed to exists, because the maximum port number is " + strconv.Itoa(server.MAXIMUM_PORT) + " .")
 	}
 }

ad620e0dd3438bc4d25e98bc84ea89afbfe3e47b
Jan Zipfler <zipfler.j@gmail.com>
2014-11-13 20:41:58 +0100

    Modified the example scripts to match the new avaStarter folder.

--- example/startController.sh
+++ example/startController.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./main -nodeList="Nodes.txt" -isController=true -ipAddress="127.0.0.1" -port=15100
+./avaStarter -nodeList="Nodes.txt" -isController=true -ipAddress="127.0.0.1" -port=15100

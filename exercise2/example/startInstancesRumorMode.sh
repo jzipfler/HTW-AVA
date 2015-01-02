6bdc7a5cd787e0c45e13f3dbced7550c3af4c172
jan <jan.zipfler@consistec.de>
2015-01-02 12:35:05 +0100

    Removed not needed files from example folder.

--- exercise2/example/startInstancesRumorMode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Do not use the 8th one because the Nodes.txt holds a error on this id.
-for i in 1 2 3 4 5; do
-	./avaStarter -nodeList="Nodes.txt" -graphvizFile="Graphviz.txt" -rumorExperiment -id ${i} > nodeOutput${i}.txt &
-done

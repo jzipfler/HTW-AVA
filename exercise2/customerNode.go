caee96e3ef09ce54443ee01a47350522bfa05d2a
jan <jan.zipfler@consistec.de>
2015-01-02 12:40:01 +0100

    Added some documentation and changed the friends map to NetworkServer

--- exercise2/customerNode.go
+++ exercise2/customerNode.go
@@ -9,10 +9,18 @@ const (
 	THRESHOLD_BUY_WHEN_FRIED_TELLS_HE_BUYED = 8
 )
 
+// This node represents a customer for exercise 2. The customer gets informed
+// from companies about new products and buys one, if he hears the advertisement
+// from a company. He also buys the product if enough friends told him, that
+// they buyed the product already.
 type CustomerNode struct {
 	server.NetworkServer
 	customerId          int
-	friends             map[int]CustomerNode //Also neighbors.
-	heardAdvertiements  map[int]int          //An entry for each company
-	heardFriedBuyedItem map[int]int          //An entry for each friend
+	friends             map[int]server.NetworkServer //Also neighbors.
+	heardAdvertiements  map[int]int                  //An entry for each company
+	heardFriedBuyedItem map[int]int                  //An entry for each friend
+}
+
+func NewCustomerNode() CustomerNode {
+	return CustomerNode{server.New(), 0, nil, nil, nil}
 }

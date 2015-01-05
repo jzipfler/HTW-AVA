cdc800a38518c4c13026b573741139b22f878321
jan <jan.zipfler@consistec.de>
2015-01-05 11:32:15 +0100

    Fixed the start scripts.

--- exercise2/example/startController.sh
+++ exercise2/example/startController.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./avaStarter -nodeList="Nodes.txt" -isController=true -ipAddress="127.0.0.1" -port=15100
+./avaStarter2 -nodeList="Nodes.txt" -isController=true -ipAddress="127.0.0.1" -port=15100
--- exercise2/example/startInstances.sh
+++ exercise2/example/startInstances.sh
@@ -55,5 +55,5 @@ exit 0
 
 # Do not use the 8th one because the Nodes.txt holds a error on this id.
 for i in 1 2 3 4 5; do
-	./avaStarter -nodeList="Nodes.txt" -graphvizFile="Graphviz.txt" -isController=false -id ${i} > nodeOutput${i}.txt &
+	./avaStarter2 -nodeList="Nodes.txt" -graphvizFile="Graphviz.txt" -isController=false -id ${i} > nodeOutput${i}.txt &
 done

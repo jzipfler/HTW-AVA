1eac510a06588e173b7764fcb3030a236fb127f8
jan <jan.zipfler@consistec.de>
2015-01-27 14:30:58 +0100

    Take care that the file is recreated if it already exists and test the new Increase and Decrease functions.

--- exercise3/fileManager/fileManager.go
+++ exercise3/fileManager/fileManager.go
@@ -16,6 +16,8 @@ var (
 	logFile     string
 	ipAddress   string
 	port        int
+	managedFile *os.File
+	force       bool
 )
 
 func init() {
@@ -24,9 +26,12 @@ func init() {
 	flag.StringVar(&logFile, "logFile", "path/to/logfile.txt", "This parameter can be used to print the logging output to the given file.")
 	flag.StringVar(&ipAddress, "ipAddress", "127.0.0.1", "The ip address of the actual starting node.")
 	flag.IntVar(&port, "port", 15100, "The port of the actual starting node.")
+	flag.BoolVar(&force, "force", false, "If force is enabled, the programm removes a existing management file and creates a new one without asking.")
 }
 
 func main() {
+
+	flag.Parse()
 
 	if filename == "path/to/file.txt" {
 		log.Printf("A filename is required.\n%s\n\n", utils.ERROR_FOOTER)
@@ -34,18 +39,50 @@ func main() {
 		os.Exit(0)
 	}
 
-	flag.Parse()
-
-	if exists := utils.CheckIfFileExists(filename); exists {
-		if writable, err := utils.CheckIfFileIsReadableAndWritebale(filename); !writable {
-			log.Fatalf("%s\n%s\n", err.Error(), utils.ERROR_FOOTER)
-		}
-	} else {
-		os.Create(filename)
-	}
-
 	utils.InitializeLogger(logFile, "")
 	utils.PrintMessage(fmt.Sprintf("File \"%s\" is now managed by this process.", filename))
+
+	if exists := utils.CheckIfFileExists(filename); exists {
+		if !force {
+			if deleteIt := askForToDeleteFile(); !deleteIt {
+				fmt.Println("Do not delete the file and exit the program.")
+				utils.PrintMessage(fmt.Sprintf("The file \"%s\" already exists and should not be deleted.", filename))
+				os.Exit(0)
+			}
+		}
+		if err := os.Remove(filename); err != nil {
+			log.Fatalf("%s\n%s\n", err.Error(), utils.ERROR_FOOTER)
+		}
+		utils.PrintMessage(fmt.Sprintf("Removed the file \"%s\"", filename))
+	}
+
+	managedFile, err := os.Create(filename)
+	utils.PrintMessage(fmt.Sprintf("Created the file \"%s\"", filename))
+	defer managedFile.Close()
+	if err != nil {
+		log.Fatalf("%s\n%s\n", err.Error(), utils.ERROR_FOOTER)
+	}
+
+	managedFile.WriteString("000000\n")
+	utils.PrintMessage("Wrote 000000 to the file.")
+
+	for i := 0; i <= 100; i++ {
+		if numbers, err := utils.IncreaseNumbersFromFirstLine(managedFile, 6); err != nil {
+			log.Fatalln(err.Error())
+		} else {
+			fmt.Println(numbers)
+		}
+	}
+
+	for i := 0; i <= 101; i++ {
+		if numbers, err := utils.DecreaseNumbersFromFirstLine(managedFile, 6); err != nil {
+			log.Fatalln(err.Error())
+		} else {
+			fmt.Println(numbers)
+		}
+	}
+
+	os.Exit(0)
 
 	serverObject := server.New()
 
@@ -59,4 +96,27 @@ func main() {
 		os.Exit(1)
 	}
 	defer server.StopServer()
+}
+
+func askForToDeleteFile() bool {
+	var input string
+	fmt.Printf("Would you like to delete the file \"%s\"? (y/j/n)", filename)
+	fmt.Print("\nInput: ")
+	if _, err := fmt.Scanln(&input); err == nil {
+		switch input {
+		case "y", "j":
+			fmt.Println("File gets deleted.")
+			return true
+		case "n":
+			fmt.Println(input)
+			return false
+		default:
+			fmt.Println("Please only insert y/j for \"YES\" or n for \"NO\".\n" + utils.ERROR_FOOTER)
+			fmt.Println("Assume a \"n\" as input.")
+			return false
+		}
+	} else {
+		fmt.Println("Please only insert y/j for \"YES\" or n for \"NO\".\n" + utils.ERROR_HEADER)
+	}
+	return false
 }

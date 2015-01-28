ae2f32ad47fe4faa939d7e6ed6227227275779d4
jan <jan.zipfler@consistec.de>
2015-01-28 16:48:19 +0100

    Enhanced the startSkript

--- exercise3/examples/startSkript.sh
+++ exercise3/examples/startSkript.sh
@@ -8,9 +8,10 @@ MANAGER_B="127.0.0.1:15101"
 
 FIRST_USER_PORT=15200
 
-./fileManager
-./fileManager
+./fileManager -filename FileA.txt -force -ipAddress ${IP} -port ${MANAGER_A_PORT} > Manager1_out.txt
+./fileManager -filename FileB.txt -force -ipAddress ${IP} -port ${MANAGER_B_PORT} > Manager2_out.txt
 
-for i in 1 2 3 4 5 6 7 8 9; do
-  ./fileUser
+for i in 1 2; do
+  CURRENT_PORT=$((${FIRST_USER_PORT}+1))
+  ./fileUser --ipAddress 127.0.0.1 -port ${CURREN_TPORT} -managerA ${MANAGER_A} -managerB ${MANAGER_B} > fileUser${1}_out.txt
 done

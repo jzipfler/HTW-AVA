743c532052f73be88eda4c17f4e325f10ac80a72
Jan Zipfler <zipfler.j@gmail.com>
2015-02-01 22:14:50 +0100

    Changed the ports because no uneven ports are allowed now.

--- exercise3/examples/startSkript.sh
+++ exercise3/examples/startSkript.sh
@@ -2,9 +2,9 @@
 
 IP="127.0.0.1"
 MANAGER_A_PORT=15100
-MANAGER_B_PORT=15101
+MANAGER_B_PORT=15102
 MANAGER_A="127.0.0.1:15100"
-MANAGER_B="127.0.0.1:15101"
+MANAGER_B="127.0.0.1:15102"
 
 FIRST_USER_PORT=15200
 
@@ -12,6 +12,6 @@ FIRST_USER_PORT=15200
 ./fileManager -filename FileB.txt -force -ipAddress ${IP} -port ${MANAGER_B_PORT} > Manager2_out.txt
 
 for i in 1 2; do
-  CURRENT_PORT=$((${FIRST_USER_PORT}+1))
+  CURRENT_PORT=$((${FIRST_USER_PORT}+2))
   ./fileUser --ipAddress 127.0.0.1 -port ${CURREN_TPORT} -managerA ${MANAGER_A} -managerB ${MANAGER_B} > fileUser${1}_out.txt
 done

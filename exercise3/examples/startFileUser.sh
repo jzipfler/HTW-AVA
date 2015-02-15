b529d2872517c767a4ccddf720b3054eb0aa651f
Jan Zipfler <zipfler.j@gmail.com>
2015-02-15 20:55:26 +0100

    Added the ability to start two different fileUsers with the same script.

--- exercise3/examples/startFileUser.sh
+++ exercise3/examples/startFileUser.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 
-./fileUser -ipAddress 127.5.5.5 -port 15200 -managerA 127.1.1.1:15100 -managerB 127.2.2.2:15102
+if test ${#} -ne 1; then
+	./fileUser -ipAddress 127.5.5.5 -port 15200 -managerA 127.1.1.1:15100 -managerB 127.2.2.2:15102
+else
+	./fileUser -ipAddress 127.6.6.6 -port 15202 -managerA 127.1.1.1:15100 -managerB 127.2.2.2:15102
+fi
